ae049bf47bf9b416caaf99665834f4927f505c93
Myron <86271481+myronladyjenko@users.noreply.github.com>
2022-02-10 05:28:46 +0300

    Time: 1 ms (76.40%), Space: 42.3 MB (5.42%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -2,20 +2,11 @@ class Solution
 {
     public boolean checkString(String s) 
     {
-        int check = 0;
-        
-        for(int i = 0; i < s.length(); i++)
+        if (s.contains("ba"))
         {
-            if (s.charAt(i)=='b')
-            {
-               check = 1;
-            }
-            if (check == 1 && s.charAt(i) == 'a')
-            {
-               return false;
-            }
+            return false;
         }
-        
+            
         return true;
     }
 }
