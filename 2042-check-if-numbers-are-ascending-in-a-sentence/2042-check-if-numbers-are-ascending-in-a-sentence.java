7476d017dfa517e77da2d37073c6b909a676b9ae
Myron <86271481+myronladyjenko@users.noreply.github.com>
2022-02-10 06:12:50 +0300

    Time: 3 ms (56.84%), Space: 41.7 MB (19.04%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -3,32 +3,32 @@ class Solution
     public boolean areNumbersAscending(String s) 
     {
         int x = 0;
-        int pre=-1;
+        int resF = -1;
         
-        for (int i = 0;i<s.length();i++)
+        for (int i = 0; i < s.length(); i++)
         {
             char c = s.charAt(i);
             
-            if(c >= '0' && c <= '9')
+            if (c >= '0' && c <= '9')
             {
                StringBuilder str= new StringBuilder();
-               int num =0;
+               int num = 0;
                 
-               while (i<=s.length()-1&&s.charAt(i)>='0'&&s.charAt(i)<='9')
+               while (i <= s.length() - 1 && s.charAt(i) >= '0' && s.charAt(i) <= '9')
                {
                    str.append(s.charAt(i));
                    i++;
                }
                 
-               num=Integer.parseInt(str.toString());
+               num = Integer.parseInt(str.toString());
                 
-               if(num<=pre)
+               if (num <= resF)
                {
-                   return false;
+                  return false;
                }
                else 
                {
-                   pre = num;
+                  resF = num;
                }
             }
         }
